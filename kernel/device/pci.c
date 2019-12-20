9acf8c2739f046f755312d721c14e5c70012d059
Totsugekitai <hrstmyk811m@gmail.com>
2019-12-20 15:21:39 +0900

    fix

--- kernel/device/pci.c
+++ kernel/device/pci.c
@@ -5,6 +5,9 @@
 #define PCI_CONFIG_ADDRESS 0xcf8
 #define PCI_CONFIG_DATA 0xcfc
 
+// AHCI Address Base
+uint64_t *abar = 0;
+
 uint16_t pci_config_read_byte(uint8_t bus, uint8_t device, uint8_t func, uint8_t offset)
 {
     uint32_t bus_number = (uint32_t)bus;
@@ -39,6 +42,23 @@ uint16_t pci_config_read_word(uint8_t bus, uint8_t device, uint8_t func, uint8_t
     return (uint16_t)((io_in32(PCI_CONFIG_DATA) >> ((offset & 2) * 8)) & 0xffff);
 }
 
+uint32_t pci_config_read_dword(uint8_t bus, uint8_t device, uint8_t func, uint8_t offset)
+{
+    uint32_t bus_number = (uint32_t)bus;
+    uint32_t device_number = (uint32_t)device;
+    uint32_t func_number = (uint32_t)func;
+
+    /* create configuration address */
+    uint32_t address = (uint32_t)((bus_number << 16) | (device_number << 11) |
+                                  (func_number << 8) | (offset & 0xfc) | ((uint32_t)0x80000000));
+
+    /* write out the address */
+    io_out32(PCI_CONFIG_ADDRESS, address);
+
+    /* read in the data */
+    return io_in32(PCI_CONFIG_DATA);
+}
+
 struct pci_config_space pci_check_parameters(uint8_t bus, uint8_t slot, uint8_t func)
 {
     struct pci_config_space config;
@@ -46,22 +66,21 @@ struct pci_config_space pci_check_parameters(uint8_t bus, uint8_t slot, uint8_t 
     /* vendors that == 0xffff, it must be a non-existent device. */
     if ((config.vendor = pci_config_read_word(bus, slot, func, 0x00)) != 0xffff) {
         config.device = pci_config_read_word(bus, slot, func, 0x02);
-        // config.command = pci_config_read_word(bus, slot, func, 0x04);
+        config.command = pci_config_read_word(bus, slot, func, 0x04);
         config.status = pci_config_read_word(bus, slot, func, 0x06);
-        // config.revision_id = pci_config_read_byte(bus, slot, func, 0x08);
+        config.revision_id = pci_config_read_byte(bus, slot, func, 0x08);
         config.prog_if = pci_config_read_byte(bus, slot, func, 0x09);
         config.subclass = pci_config_read_byte(bus, slot, func, 0x0a);
         config.class_code = pci_config_read_byte(bus, slot, func, 0x0b);
-        // config.cache_line_size = pci_config_read_byte(bus, slot, func, 0x0c);
-        // config.latency_timer = pci_config_read_byte(bus, slot, func, 0x0d);
+        config.cache_line_size = pci_config_read_byte(bus, slot, func, 0x0c);
+        config.latency_timer = pci_config_read_byte(bus, slot, func, 0x0d);
         config.header_type = pci_config_read_byte(bus, slot, func, 0x0e);
-        // config.bist = pci_config_read_byte(bus, slot, func, 0x0f);
+        config.bist = pci_config_read_byte(bus, slot, func, 0x0f);
 
         config.capabilities_pointer = pci_config_read_byte(bus, slot, func, 0x34);
 
-        // config.interrupt_line = pci_config_read_byte(bus, slot, func, 0x3c);
-        // config.interrupt_pin = pci_config_read_byte(bus, slot, func, 0x3d);
-        config.address_map_register = pci_config_read_word(bus, slot, func, 0x90);
+        config.interrupt_line = pci_config_read_byte(bus, slot, func, 0x3c);
+        config.interrupt_pin = pci_config_read_byte(bus, slot, func, 0x3d);
     }
     return config;
 }
@@ -73,22 +92,48 @@ void check_all_buses(void)
         for (int device = 0; device < 32; device++) {
             for (int func = 0; func < 8; func++) {
                 config = pci_check_parameters(bus, device, func);
+                // if device found
                 if (config.vendor != 0xffff) {
-                    put_str_num_serial("bus: ", bus);
-                    put_str_num_serial("device: ", device);
-                    put_str_num_serial("function: ", func);
-                    put_str_num_serial("vendor: ", config.vendor);
-                    put_str_num_serial("device: ", config.device);
-                    put_str_num_serial("status: ", config.status);
-                    put_str_num_serial("prog_if: ", config.prog_if);
-                    put_str_num_serial("subclass: ", config.subclass);
-                    put_str_num_serial("class_code: ", config.class_code);
-                    put_str_num_serial("header_type: ", config.header_type);
-                    put_str_num_serial("capabilities_pointer: ", config.capabilities_pointer);
-                    put_str_num_serial("address_map_register: ", config.address_map_register);
-                    puts_serial("\n");
+                    print_device_info(bus, device, func, config);
+                }
+                // if AHCI found
+                if (config.class_code == 0x01 && config.subclass == 0x06) {
+                    abar = get_ahci_base_address(bus, device, func);
+                    put_str_num_serial("ABAR: ", (uint64_t)abar);
                 }
             }
         }
     }
+}
+
+void print_device_info(int bus, int device, int func, struct pci_config_space config)
+{
+    put_str_num_serial("bus: ", bus);
+    put_str_num_serial("device: ", device);
+    put_str_num_serial("function: ", func);
+
+    put_str_num_serial("vendor: ", config.vendor);
+    put_str_num_serial("device: ", config.device);
+    put_str_num_serial("command: ", config.command);
+    put_str_num_serial("status: ", config.status);
+    put_str_num_serial("revision_id: ", config.revision_id);
+    put_str_num_serial("prog_if: ", config.prog_if);
+    put_str_num_serial("subclass: ", config.subclass);
+    put_str_num_serial("class_code: ", config.class_code);
+    put_str_num_serial("cache_line_size: ", config.cache_line_size);
+    put_str_num_serial("latency_timer: ", config.latency_timer);
+    put_str_num_serial("header_type: ", config.header_type);
+    put_str_num_serial("bist: ", config.bist);
+
+    put_str_num_serial("capabilities_pointer: ", config.capabilities_pointer);
+
+    put_str_num_serial("interrupt_line: ", config.interrupt_line);
+    put_str_num_serial("interrupt_pin: ", config.interrupt_pin);
+
+    puts_serial("\n");
+}
+
+uint64_t *get_ahci_base_address(int bus, int device, int func)
+{
+    return (uint64_t *)(uint64_t)pci_config_read_dword(bus, device, func, 0x24);
 }
--- kernel/include/device/device.h
+++ kernel/include/device/device.h
@@ -37,91 +37,9 @@ struct pci_config_space {
 
     uint8_t interrupt_line;
     uint8_t interrupt_pin;
-
-    uint16_t address_map_register;
 };
+
 struct pci_config_space pci_check_parameters(uint8_t bus, uint8_t slot, uint8_t func);
 void check_all_buses(void);
-
-/* Serial-ATA */
-struct generic_host_ctl;
-struct port_reg;
-struct cmd_header;
-struct cmd_list;
-struct phy_region_desc_tbl;
-
-struct hba_mem_regs {
-    struct generic_host_ctl generic_host_ctl;
-    uint8_t reserved[0xa0-0x2c];
-    uint8_t vsrs[0x100-0xa0];
-    struct port_reg ports[32];
-};
-
-struct generic_host_ctl {
-    uint32_t hba_cap;
-    uint32_t globl_hba_ctl;
-    uint32_t int_stat;
-    uint32_t port_impl;
-    uint32_t version;
-    uint32_t ccc_ctl;
-    uint32_t ccc_ports;
-    uint32_t em_loc;
-    uint32_t em_ctl;
-    uint32_t hba_cap2;
-    uint32_t bohc;
-};
-
-struct port_reg {
-    uint32_t clb;
-    uint32_t clbu;
-    uint32_t fb;
-    uint32_t fbu;
-    uint32_t is;
-    uint32_t ie;
-    uint32_t cmd;
-    uint32_t reserved0;
-    uint32_t tfd;
-    uint32_t sig;
-    uint32_t ssts;
-    uint32_t serr;
-    uint32_t sact;
-    uint32_t ci;
-    uint32_t sntf;
-    uint32_t fbs;
-    uint32_t reserved1[11];
-    uint32_t vs[4];
-};
-
-struct cmd_list {
-    struct cmd_header headers[32];
-};
-
-struct cmd_header {
-    // DW0
-    uint32_t flags;
-    // DW1
-    uint32_t prdbc;
-    // DW2-3
-    uint32_t ctba0;
-    uint32_t ctba_u0;
-    // DW4-7
-    uint32_t reserved[4];
-};
-
-struct cmd_tbl {
-    // 0x00
-    uint8_t cfis[64];
-    // 0x40
-    uint8_t acmd[16];
-    // 0x50
-    uint8_t reserved[48];
-    // 0x80
-    struct phy_region_desc_tbl prdt_entry[1];
-};
-
-struct phy_region_desc_tbl {
-    uint32_t dba;
-    uint32_t dbau;
-    uint32_t reserved0;
-    uint32_t flags;
-};
+void print_device_info(int bus, int device, int func, struct pci_config_space config);
+uint64_t *get_ahci_base_address(int bus, int device, int func);
